e4022b8b5570aeeb779a628ecac399beb81b0a08
mochi-dot-py <65631838+mochi-dot-py@users.noreply.github.com>
2020-10-26 19:21:26 -0400

    Enabled displaying of data in db.json through api/notes

--- server.js
+++ server.js
@@ -19,15 +19,21 @@ app.get("/notes", function (req, res) {
 });
 
 // GET API notes route
-app.get("api/notes", function (req, res) {});
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    text = JSON.parse(data);
+    res.send(text);
+  });
+});
 
 // POST API notes route
-app.post("api/notes", function (req, res) {});
+app.post("/api/notes", function (req, res) {});
 
 // DELETE API notes route
-app.delete("api/notes:id", function (req, res) {});
+app.delete("/api/notes:id", function (req, res) {});
 
 // Port listener
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
+app.listen(PORT, () => console.log("App listening on PORT " + PORT));
